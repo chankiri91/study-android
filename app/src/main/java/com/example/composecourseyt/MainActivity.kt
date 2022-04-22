3ec7be1e594210f61f220e314321612d3cc8531f
kiri-91 <kirifog91@gmail.com>
2022-04-22 22:20:47 +0900

    Modifierの記述は上から適応される

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -3,31 +3,39 @@ package com.example.composecourseyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import java.lang.reflect.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "kiri")
+            Column(
+                modifier = androidx.compose.ui.Modifier
+                    .fillMaxHeight(0.5f)
+                    .width(300.dp)
+                    .background(Color.Green)
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Red)
+                    .padding(10.dp)
+            ) {
+                Text(text = "Hello",
+                    modifier = androidx.compose.ui.Modifier
+                        .offset(20.dp, 20.dp)
+                        .padding(20.dp)
+                        .border(5.dp, Color.Black)
+                )
+                Spacer(modifier = androidx.compose.ui.Modifier.height(200.dp))
+                Text(text = "World")
+            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeCourseYTTheme {
-        Greeting("Android")
     }
 }
