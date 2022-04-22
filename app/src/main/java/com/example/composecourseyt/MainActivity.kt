0ab5e65a013f6254ff57c8841ca164becf3687c6
kiri-91 <kirifog91@gmail.com>
2022-04-22 13:38:28 +0900

    learn setContent Row Column

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -3,31 +3,39 @@ package com.example.composecourseyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "kiri")
+            // Command + P で何が入るやつか見れる
+            Row(
+                modifier = Modifier
+                    .width(300.dp)
+                    .height(300.dp)
+                    // .fillMaxSize()
+                    .background(Color.Yellow),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+                /* horizontalAllignment = Allignment.Alignment.CenterVertically
+                verticalArrangement = Arrangement.SpaceAround */
+            ) {
+                Text(text = "Hello")
+                Text(text = "World")
+                Text(text = "hello")
+            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeCourseYTTheme {
-        Greeting("Android")
     }
 }
