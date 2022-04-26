e0967dab1dd5d3fdac27b2b5598dc04daaec09f9
kiri-91 <kirifog91@gmail.com>
2022-04-26 20:36:09 +0900

    rememberInfiniteTransition

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -4,7 +4,8 @@ import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
@@ -27,19 +28,49 @@ class MainActivity : ComponentActivity() {
                 mutableStateOf(200.dp)
             }
             val size by animateDpAsState(
-                targetValue = sizeState
+                targetValue = sizeState,
+                /*
+                tween(
+                    durationMillis = 3000,
+                    delayMillis = 300,
+                    easing = LinearOutSlowInEasing
+                )
+
+                spring(
+                    Spring.DampingRatioHighBouncy
+                )
+
+                keyframes {
+                    durationMillis = 5000
+                    sizeState at 0 with LinearEasing
+                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
+                    sizeState * 2f at 5000
+                }
+                 */
+                tween(
+                    durationMillis = 5000
+                )
+            )
+            val infiniteTransition = rememberInfiniteTransition()
+            val color by infiniteTransition.animateColor(
+                initialValue = androidx.compose.ui.graphics.Color.Red,
+                targetValue = androidx.compose.ui.graphics.Color.Green,
+                animationSpec = infiniteRepeatable(
+                    tween(2000),
+                    repeatMode = RepeatMode.Reverse
+                )
             )
 
             Box(
                 modifier = Modifier
                     .size(size)
-                    .background(androidx.compose.ui.graphics.Color.Red),
+                    .background(color),
                 contentAlignment = Alignment.Center
             ) {
                 Button(onClick = {
                     sizeState += 50.dp
                 }) {
-                    Text(text = "Increse Size")
+                    Text(text = "Increase Size")
                 }
             }
         }
