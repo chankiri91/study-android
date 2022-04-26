f87fecc65b9f61b1e41050e885f694aef432cc1b
kiri-91 <kirifog91@gmail.com>
2022-04-26 19:53:05 +0900

    by animateDpAsStateやばい

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -1,14 +1,47 @@
 package com.example.composecourseyt
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            
+            // importのruntime.rememberをruntime.*nにする
+            var sizeState by remember {
+                mutableStateOf(200.dp)
+            }
+            val size by animateDpAsState(
+                targetValue = sizeState
+            )
+
+            Box(
+                modifier = Modifier
+                    .size(size)
+                    .background(androidx.compose.ui.graphics.Color.Red),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(onClick = {
+                    sizeState += 50.dp
+                }) {
+                    Text(text = "Increse Size")
+                }
+            }
         }
     }
 }
